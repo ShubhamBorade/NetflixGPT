2f6307dc77ddcfcca756e7f0aeaa3f116e389771
Shubham <shubhamborade14899@gmail.com>
2023-09-16 12:00:37 +0530

    opneai key issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,4 +20,4 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+export const OPENAI_KEY = "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";
